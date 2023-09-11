35f822720665d2a5b352138b7517c5b27bc306fe
y72wvh <betty.becuwe@insee.fr>
2023-09-11 11:22:28 +0200

    feat: add healthcheck

---
+++ src/main/java/fr/insee/survey/datacollectionmanagement/heathcheck/controller/HealthcheckController.java
@@ -0,0 +1,20 @@
+package fr.insee.survey.datacollectionmanagement.heathcheck.controller;
+
+import fr.insee.survey.datacollectionmanagement.constants.Constants;
+import fr.insee.survey.datacollectionmanagement.heathcheck.dto.HealthcheckDto;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "8 - Heathckeck", description = "healthckeck")
+public class HealthcheckController {
+
+    @GetMapping(path = Constants.API_HEALTHCHECK, produces = "application/json")
+    public ResponseEntity<HealthcheckDto> healtcheck() {
+        HealthcheckDto dto = new HealthcheckDto();
+        dto.setStatus("OK");
+        return ResponseEntity.ok().body(dto);
+    }
+}
