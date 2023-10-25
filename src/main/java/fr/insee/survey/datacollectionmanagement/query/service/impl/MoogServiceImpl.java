5466ab8e4bcbaa6af31d34ad0873e5e29d728e52
y72wvh <betty.becuwe@insee.fr>
2023-10-25 15:32:48 +0200

    feat: add moog endpoint for readonly url in moog

--- src/main/java/fr/insee/survey/datacollectionmanagement/query/service/impl/MoogServiceImpl.java
+++ src/main/java/fr/insee/survey/datacollectionmanagement/query/service/impl/MoogServiceImpl.java
@@ -4,8 +4,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
+import fr.insee.survey.datacollectionmanagement.config.ApplicationConfig;
 import fr.insee.survey.datacollectionmanagement.config.JSONCollectionWrapper;
+import fr.insee.survey.datacollectionmanagement.metadata.domain.Partitioning;
+import fr.insee.survey.datacollectionmanagement.metadata.service.PartitioningService;
 import fr.insee.survey.datacollectionmanagement.query.dto.MoogExtractionRowDto;
+import fr.insee.survey.datacollectionmanagement.questioning.domain.Questioning;
+import fr.insee.survey.datacollectionmanagement.questioning.service.QuestioningService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
@@ -21,8 +27,10 @@ import fr.insee.survey.datacollectionmanagement.query.repository.MoogRepository;
 import fr.insee.survey.datacollectionmanagement.query.service.MoogService;
 import fr.insee.survey.datacollectionmanagement.view.domain.View;
 import fr.insee.survey.datacollectionmanagement.view.service.ViewService;
+import org.webjars.NotFoundException;
 
 @Service
+@Slf4j
 public class MoogServiceImpl implements MoogService {
 
     @Autowired
@@ -36,6 +44,15 @@ public class MoogServiceImpl implements MoogService {
 
     @Autowired
     private MoogRepository moogRepository;
+
+    @Autowired
+    QuestioningService questioningService;
+
+    @Autowired
+    PartitioningService partitioningService;
+
+    @Autowired
+    ApplicationConfig applicationConfig;
 
     @Override
     public List<View> moogSearch(String field) {
@@ -106,5 +123,30 @@ public class MoogServiceImpl implements MoogService {
     public Collection<MoogExtractionRowDto> getSurveyUnitsToFollowUp(String idCampaign) {
         return moogRepository.getSurveyUnitToFollowUp(idCampaign);
     }
+
+    @Override
+    public String getReadOnlyUrl(String idCampaign, String surveyUnitId) throws NotFoundException {
+        Optional<Campaign> campaign = campaignService.findById(idCampaign);
+        if (!campaign.isPresent()) {
+            throw new NotFoundException("Campaign not found");
+        }
+        Set<Partitioning> setParts = campaign.get().getPartitionings();
+        Questioning questioning = null;
+        for (Partitioning part : setParts){
+            Questioning qTemp = questioningService.findByIdPartitioningAndSurveyUnitIdSu(part.getId(), surveyUnitId);
+            if(qTemp!=null){
+                questioning =qTemp;
+                break;
+            }
+        }
+        if(questioning!=null) {
+            return applicationConfig.getQuestioningUrl() + "/readonly/questionnaire/" + questioning.getModelName()
+                    + "/unite-enquetee/" + surveyUnitId;
+        }
+        String msg = "0 questioning found for campaign "+idCampaign+" and survey unit "+ surveyUnitId;
+        log.error(msg);
+        throw new  NotFoundException(msg);
+    }
+
 
 }
---
+++ src/test/java/fr/insee/survey/datacollectionmanagement/query/controller/MoogControllerTest.java
@@ -0,0 +1,53 @@
+package fr.insee.survey.datacollectionmanagement.query.controller;
+
+import fr.insee.survey.datacollectionmanagement.constants.Constants;
+import fr.insee.survey.datacollectionmanagement.query.service.CheckHabilitationService;
+import fr.insee.survey.datacollectionmanagement.query.service.MoogService;
+import fr.insee.survey.datacollectionmanagement.questioning.domain.SurveyUnit;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles("test")
+public class MoogControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MoogService moogService;
+
+    @Test
+    public void getMoogReadOnlyUrl() throws Exception {
+        String idCampaign= "SOURCE12023T01";
+        String surveyUnitId= "100000000";
+        this.mockMvc.perform(get(Constants.MOOG_API_READONLY_URL, idCampaign,surveyUnitId)).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string("http://localhost:8081/readonly/questionnaire/m0/unite-enquetee/100000000"));
+    }
+
+
+    @Test
+    public void getMoogReadOnlyUrlCampaignNotFound() throws Exception {
+        String idCampaign= "SOURCE12023T00";
+        String surveyUnitId= "100000000";
+        this.mockMvc.perform(get(Constants.MOOG_API_READONLY_URL, idCampaign,surveyUnitId)).andDo(print()).andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void getMoogReadOnlyUrlQuestioningNotFound() throws Exception {
+        String idCampaign= "SOURCE12023T01";
+        String surveyUnitId= "10";
+        this.mockMvc.perform(get(Constants.MOOG_API_READONLY_URL, idCampaign,surveyUnitId)).andDo(print()).andExpect(status().isNotFound());
+    }
+}
