95e981d96a8f0f2f6f2c031710db181bfd38682d
y72wvh <betty.becuwe@insee.fr>
2024-03-06 17:03:05 +0100

    fix: path names

--- src/main/java/fr/insee/survey/datacollectionmanagement/constants/Constants.java
+++ src/main/java/fr/insee/survey/datacollectionmanagement/constants/Constants.java
@@ -86,10 +86,10 @@ public class Constants {
     public static final String API_CONTACTS_SEARCH = "/api/contacts/search";
     public static final String API_CONTACTS_ACCREDITATIONS = "/api/contacts/{id}/accreditations";
     public static final String API_MY_QUESTIONINGS_ID = "/api/contacts/questionings";
-    public static final String API_SURVEY_UNITS_CONTACTS = "/api/survey_units/{id}/contacts";
-    public static final String API_SURVEY_UNITS_PARTITIONINGS = "/api/survey_units/{id}/partitionings";
+    public static final String API_SURVEY_UNITS_CONTACTS = "/api/survey-units/{id}/contacts";
+    public static final String API_SURVEY_UNITS_PARTITIONINGS = "/api/survey-units/{id}/partitionings";
 
-    public static final String API_SURVEY_UNITS_SEARCH = "/api/survey_units/search";
+    public static final String API_SURVEY_UNITS_SEARCH = "/api/survey-units/search";
 
 
 
