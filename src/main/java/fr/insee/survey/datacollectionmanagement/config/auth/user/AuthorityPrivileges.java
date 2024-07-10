ab44b7c6d85ecc27100210a7e35b470f9fd767ac
y72wvh <betty.becuwe@insee.fr>
2024-07-10 09:18:49 +0200

    fix: authentication name case insensitive

--- src/main/java/fr/insee/survey/datacollectionmanagement/config/auth/user/AuthorityPrivileges.java
+++ src/main/java/fr/insee/survey/datacollectionmanagement/config/auth/user/AuthorityPrivileges.java
@@ -6,8 +6,8 @@ public class AuthorityPrivileges {
     }
 
     public static final String HAS_MANAGEMENT_PRIVILEGES = "hasAnyRole('INTERNAL_USER', 'WEB_CLIENT', 'ADMIN')";
-    public static final String HAS_REPONDENT_PRIVILEGES = "hasAnyRole('RESPONDENT')";
-    public static final String HAS_REPONDENT_LIMITATED_PRIVILEGES = "hasRole('RESPONDENT') && #id == authentication.name ";
+    public static final String HAS_REPONDENT_PRIVILEGES = "hasRole('RESPONDENT')";
+    public static final String HAS_REPONDENT_LIMITATED_PRIVILEGES = "hasRole('RESPONDENT') && #id.toLowerCase() == authentication.name.toLowerCase() ";
     public static final String HAS_ADMIN_PRIVILEGES = "hasAnyRole('WEB_CLIENT', 'ADMIN)";
     public static final String HAS_USER_PRIVILEGES = "hasAnyRole('INTERNAL_USER', 'WEB_CLIENT', 'RESPONDENT', 'ADMIN')";
 
